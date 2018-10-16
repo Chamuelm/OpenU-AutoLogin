7eea0baf276d83e9fae09c8ee3d21cdd60d9b58e
chamuelm <chamuelm@gmail.com>
2018-10-17 00:49:41 +0300

    More changes to code logic

--- options.js
+++ options.js
@@ -1,18 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
   var submitBtn = document.getElementById('submitButton');
   // onClick's logic below:
-  submitBtn.addEventListener('click', saveChanges());
+  submitBtn.addEventListener('click', saveChanges);
 });
 
 function saveChanges() {
         let openu_id_temp = document.getElementById('openu_id');
         // Check that there's some code there.
         if (!openu_id_temp) {
-          message('Error: Missing value');
+          alert('Error: Missing value');
           return;
         }
 
-        chrome.storage.sync.set({'openu_id_storage': openu_id_temp.value}, function() {
-          message('Settings saved');
+        chrome.storage.sync.set({openu_id_storage: openu_id_temp.value}, function() {
+          alert('Settings saved');
         });
       }
--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-chrome.storage.sync.get('openu_id', function(result) {
-    document.getElementById("p_mis_student").value = result.openu_id;
-  });
-
+chrome.storage.sync.get(['openu_id_storage'], function(result) {
+  document.getElementById("p_mis_student").value = result.openu_id;
+});
 document.getElementById("login_sso").submit();
